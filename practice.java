d0e1b758989240ae0f16bc0d975dfddbd5d5a564
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-27 12:27:52 +0530

    practice datatypes in java

---
+++ practice.java
@@ -0,0 +1,57 @@
+class ABC{
+    public static void main(String args[]){
+    //  int a=3;
+    //  int b=6;
+    //  System.out.println("addition of "+a+" and "+b+" is :"+(a+b));
+  
+
+      //Range of byte is (-128 to 127)
+
+    //  byte a= 127;
+    //  System.out.println("Byte number: " +a);
+
+    //  byte b= (byte)128;
+    //  System.out.println("Byte number: " +b);
+
+    // byte c= (byte)140;
+    //  System.out.println("Byte number: " + c);
+
+    //  short s = (short)82382;
+    //  System.out.println("Short number :"+s);
+
+    //  int x = (short)82382;
+    //  System.out.println("Short number :"+(short)x);
+
+    //  long l= 38389258233823L;
+    //  System.out.println("long number is:"+l); 
+
+    //  long p= 383892582;   
+    //  System.out.println("long number is:"+p);
+
+    // float f = 2.5f;
+    // System.out.println("Floating number is :"+f);
+   
+    //  int a = 20;
+    //  float b = 37.3f;
+    //  System.out.println("addition of "+a+" and "+b+" is "+(a+b));
+
+   //  char = ' '
+   // String = " "
+
+    // char ch = 'A';
+    // System.out.println("charecter is :"+ch);  //A
+    // System.out.println("charecter is :"+(ch+1));  //66
+    // System.out.println("charecter is :"+ (int)ch); //65
+    //C  //B
+
+    // int a = 4;
+    // int b = 9;
+    // boolean z = a>b;
+    // System.out.println("boolean is :"+z); //false
+
+    // while(false){  // unreachable statement
+    //      System.out.println("hello");
+    // }
+
+    }
+}
