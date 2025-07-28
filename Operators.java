4b7e0b547a6e7b0618caeb45b9256ec9ff3b5882
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-28 23:14:36 +0530

    Realtional operator

--- Operators.java
+++ Operators.java
@@ -8,32 +8,63 @@
 
 
  //ARITHMETIC OPERATOR
- class operator1{
-     public static void main(String ajhd[]){
-         int a = 5;
-         int b = 94;
-         System.out.println("Addition of "+a+" and "+b+" is "+(a+b));
-         System.out.println("Subtraction of "+a+" and "+b+" is "+(a-b));           
-         System.out.println("Multiplication of "+a+" and "+b+" is "+(a*b));           
-         System.out.println("Division of "+a+" and "+b+" is "+(b/a));           
-         System.out.println("Modulo of "+a+" and "+b+" is "+(b%a));           
+//  class operator1{
+//      public static void main(String ajhd[]){
+//          int a = 5;
+//          int b = 94;
+//          System.out.println("Addition of "+a+" and "+b+" is "+(a+b));
+//          System.out.println("Subtraction of "+a+" and "+b+" is "+(a-b));           
+//          System.out.println("Multiplication of "+a+" and "+b+" is "+(a*b));           
+//          System.out.println("Division of "+a+" and "+b+" is "+(b/a));           
+//          System.out.println("Modulo of "+a+" and "+b+" is "+(b%a));           
 
-      short x= 5;
-      short y = 10;
-     short z = x*y;   // return int 
-      System.out.println("multiplication of short num is :"+(x*y));
+//       short x= 5;
+//       short y = 10;
+//       short z = (short)x*y;   // return int 
+//       System.out.println("multiplication of short num is :"+z);
 
-      String name1 = "Hello ";
-      String name2 = "Divya";
-      System.out.println("Concatination of Strings :" + (name1+name2));
+//       String name1 = "Hello ";
+//       String name2 = "Divya";
+//       System.out.println("Concatination of Strings :" + (name1+name2));
 
 
-    int m = 3;
-    int n = 2;
-    System.out.println("total is : "+m+n);  //total is : 32(concatinate)
+//       int m = 3;
+//       int n = 2;
+//       System.out.println("total is : "+m+n);  //total is : 32(concatinate)
 
-    // int num = 10;
-    // System.out.println("Division is :"+(num/0));  // abort program (runtime error)
+//       int num = 10;
+//       System.out.println("Division is :"+(num/0));  // abort program (runtime error)
 
-     }
+//      }
+//  }
+
+
+ class operator2{
+    public static void main(String []args){
+       //RELATIONAL OPERATOR(return boolean value) 
+         // int A = 10;
+         // int B = 23;
+        // boolean C = A > B;
+        // System.out.println("result is :"+C);
+
+
+        //LOGICAL OPERATOR
+          int x = 5;
+          int y = 2;
+          boolean z = ( x > y ) && ( x != y );  // 5 > 2 && 5 != 2 => true  && true => true
+          System.out.println("result is :"+z); // true
+        
+          System.out.println(x > y && ++x < y); // 5 > 2 && 6 < 2 => true && false => false
+           System.out.println("x= "+x+" y= "+y); 
+           //x = 6, y = 2
+          System.out.println(x > y && x < y++); // 6>2 && 6 < 2 => true && false =>false
+           //x = 6, y = 3
+          System.out.println(x < y && x < --y);// 6 < 3 false (second condion not check)
+          System.out.println(x != y && x-- > y); // 6 != 3 && 6>3 =>true && true => true
+           //x = 5, y = 2
+          System.out.println(x == y && x-- > --y); // 5 == 2 => false(second condition not check)
+    }
  }
+
+
+
