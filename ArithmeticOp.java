3f9ff97970c2e46008fa393a6bdaa959d1307ffe
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-31 11:46:47 +0530

    About Operators in Java

---
+++ ArithmeticOp.java
@@ -0,0 +1,40 @@
+// 1. logical operator
+// 2. arithmetic operator
+// 3. relational operator
+// 4. bitwise operator
+// 5. assignment operator
+// 6. ternary operator
+// 7. increment decrement operator
+
+
+ //ARITHMETIC OPERATOR
+ class operator1{
+     public static void main(String ajhd[]){
+         int a = 5;
+         int b = 94;
+         System.out.println("Addition of "+a+" and "+b+" is "+(a+b)); 
+         System.out.println("Subtraction of "+a+" and "+b+" is "+(a-b));          
+         System.out.println("Multiplication of "+a+" and "+b+" is "+(a*b));           
+         System.out.println("Division of "+a+" and "+b+" is "+(b/a));           
+         System.out.println("Modulo of "+a+" and "+b+" is "+(b%a));           
+
+      short x= 5;
+      short y = 10;
+      short z = (short)(x*y);   // return int 
+      System.out.println("multiplication of short num is :"+z);
+
+      String name1 = "Hello ";
+      String name2 = "Divya";
+      System.out.println("Concatination of Strings :" + (name1+name2));
+
+
+      int m = 3;
+      int n = 2;
+      System.out.println("total is : "+m+n);  //total is : 32(concatinate)
+
+      int num = 10;
+      System.out.println("Division is :"+(num/0));  // abort program (runtime error)
+
+     }
+ }
+
---
+++ ternaryOp.java
@@ -0,0 +1,17 @@
+// TERNARY OPERATOR
+class operator3{
+    public static void main(String args[]){
+        int a = 5;
+        int b = 2;
+        int c = (a>b)? a : b;
+        System.out.println("max is :"+c);
+
+        int p = 43;
+        int q = 93;
+        int r = 23;
+        int res;
+        res = ( p>q ) ? ( p>r ? p : r ) : ( q>r ? q : r );
+        System.out.println("result is :"+res);
+
+    }
+}
