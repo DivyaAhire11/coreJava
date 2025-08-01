da6ca2afbfe3543e14b82a6999960dbe69cd7cc3
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-01 15:19:39 +0530

    constructor practice

--- construct.java
+++ construct.java
@@ -1,17 +1,23 @@
-class ABC{
-  ABC(){
-    System.out.println("I am a Constructor");
-  }
-  public void m1(){
-    System.out.println("m1() method");
+class student {
+  private String name;
+  private float mks;
+  private int roll;
+
+  student(){
+    name = "hello";
+    roll = 50;
+    mks = 35.50f;
   }
 
+  public void display(){
+    System.out.println(name); 
+    System.out.println(mks); 
+    System.out.println(roll);
+  }
 }
- class testABC{
-    public static void main(String args[]){
-        ABC ob = new ABC(); 
-        ob.m1();
-                
-      }
-     
- }
+class testStudent{
+  public static void main(String[] args) {
+     student ob = new student();
+     ob.display();
+  }
+}
