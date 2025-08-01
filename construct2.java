ee14e9c216821d658d6093f8d1d65bf55a4695a1
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-01 15:59:58 +0530

    modified

--- construct2.java
+++ construct2.java
@@ -1,25 +1,30 @@
-class A{
-    A(){
+class A {
+    A() {
         this(10, "ritu");
-        System.out.println("Constuctor with no parameter");//2nd
+        System.out.println("Constuctor with no parameter");// 2nd
     }
-    A(int x){
-        this();
-        System.out.println("one parameter Constuctor "+x); //3rd
+
+    A(int x) {
+        this(); // constuctor call explicitly with help of this()
+        System.out.println("one parameter Constuctor " + x); // 3rd
     }
-    A(int x,String y){
-        System.out.println("two parameter Constuctor ");//1 execute
+
+    A(int x, String y) {
+        System.out.println("two parameter Constuctor ");// 1 execute
     }
-    void m1(){
-        System.out.println("m1() method");//4rth
+
+    void m1() {
+        System.out.println("m1() method");// 4rth
     }
-    void m2(){
+
+    void m2() {
         m1();
-        System.out.println("m2() method");//5th
+        System.out.println("m2() method");// 5th
     }
 
 }
-class AAA{
+
+class AAA {
     public static void main(String[] args) {
         A ob = new A(34);
         ob.m2();
