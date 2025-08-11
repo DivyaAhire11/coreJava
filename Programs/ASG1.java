3578e5df3f78489763f73ad94e204bbf16b8d7da
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-11 17:08:34 +0530

    ASG1 Added

--- Programs/ASG1.java
+++ Programs/ASG1.java
@@ -4,11 +4,12 @@ public class ASG1{
     public static void main(String args[])throws IOException{
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        while(true){
-        System.out.println("Choose Any Choice");
+        System.out.println("\nChoose Any Choice");
         System.out.println("1 : Volume of Cylender ");// V = pi*r*r*h
         System.out.println("2 : Factorial ");
         System.out.println("3 : Armstrong ");
         System.out.println("4 : Exit");
+        System.out.println("----------------------");
         System.out.println("Enter Your Choice : ");
 
         int ch  =Integer.parseInt(br.readLine());
@@ -21,6 +22,33 @@ public class ASG1{
                     Float V = 22/7*r*r*h;
                     System.out.println("Volume of cylinder is : "+V);
                     break;
+
+            case 2 : System.out.print("Enter Number : ");
+                    int n = Integer.parseInt(br.readLine());
+                    int fact=1;
+                    for(int i = 1 ; i<=n ; i++){
+                        fact = fact * i; 
+                    }
+                    System.out.println("factorial of "+n+" is : "+ fact);
+                    break;
+
+            case 3 : System.out.print("Enter Number : ");
+                    int no = Integer.parseInt(br.readLine());
+                    int num, rem,result =0;
+                    int numDigits = String.valueOf(no).length();
+                   num = no;
+                    while(num != 0){
+                        rem = num %10;;
+                        result += Math.pow(rem,numDigits);
+                        num /= 10;
+                    }
+                    if(result == no)
+                       System.out.println(no +" is an Armstrong number");
+                    else 
+                       System.out.println(no +" is not an Armstrong number");
+                    break;
+            case 4 : System.exit(0);
+            default : System.out.println("InValid choice");
        }
        }
     }
