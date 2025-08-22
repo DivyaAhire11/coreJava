37d270250856b4d07abbd05b4fb06ab05f90e4b5
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-22 23:36:30 +0530

    Array of an object practice

--- Practice/InfoAccept/Flight1.java
+++ Practice/InfoAccept/Flight1.java
@@ -29,7 +29,7 @@ class Flight{
 }
 class testFlight{
     public static void main(String []hdc){
-       
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // Flight ob1 = new Flight(); //single object create
         // ob1.accept();
         // ob1.display();      
@@ -38,7 +38,24 @@ class testFlight{
         // ob2.accept();
         // ob2.display();
 
-       
-        
+        //array of an object
+        int n=0,i;
+        System.out.print("Enter How many Flights : ");
+        try{
+        n = Integer.parseInt(br.readLine());
+        }catch(IOException e){ }
+
+        Flight[] arr = new Flight[n];
+        for(i=0;i<arr.length;i++){
+            arr[i] = new Flight();
+            System.out.println("\n Enter for "+ (i+1) +" Flight Info");
+            arr[i].accept();
+        }
+
+        System.out.println("All Info About Fligts Are Follow");
+        System.out.println("FLIGHT\t\tAIRLINE\t\tDURATION");
+        for(Flight f : arr){
+            f.display();
+        }
     }
 }
