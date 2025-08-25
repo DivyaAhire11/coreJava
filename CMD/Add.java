d16aa7807b8e0994c14a0c0595c4e15be2e999ca
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-25 20:35:27 +0530

    Using Command line argument

---
+++ CMD/Add.java
@@ -0,0 +1,14 @@
+class Add{
+    public static void main(String args[]){
+        try{
+            System.out.println("First Input is :"+args[0]);//4
+            System.out.println("Second Input is :"+args[1]);//6
+            System.out.println("Output is :"+(args[0]+args[1])); //46
+
+        }catch(Exception e){
+            System.out.println("Something Happened :"+e);
+        }
+       
+              
+    }
+}
