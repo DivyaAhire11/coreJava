2f34f421186cb2029d91dd58b3daf8f77fc5bd31
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-25 20:44:32 +0530

    related some problems

--- CMD/Add.java
+++ CMD/Add.java
@@ -1,9 +1,30 @@
+/*
+Problems while INPUT
+ 1. java Add 7 3 3
+First Input is :7
+Second Input is :3
+Output is :10
+
+2.java Add x 8  
+Something Happened :java.lang.NumberFormatException: For input string: "x"
+
+3.java Add 8 a
+Something Happened :java.lang.NumberFormatException: For input string: "a"
+
+4.java Add 8  
+Something Happened :java.lang.ArrayIndexOutOfBoundsException: Index 1 out of bounds for length 1
+*/
 class Add{
     public static void main(String args[]){
         try{
-            System.out.println("First Input is :"+args[0]);//4
-            System.out.println("Second Input is :"+args[1]);//6
-            System.out.println("Output is :"+(args[0]+args[1])); //46
+
+            int a,b;
+            a = Integer.parseInt(args[0]);
+            b = Integer.parseInt(args[1]);
+           
+            System.out.println("First Input is :"+a);//4
+            System.out.println("Second Input is :"+b);//6
+            System.out.println("Output is :"+(a+b)); //10
 
         }catch(Exception e){
             System.out.println("Something Happened :"+e);
