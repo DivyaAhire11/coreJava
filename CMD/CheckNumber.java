b64fefeacadf879b400c7bfd62b6fe215eb45842
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-29 17:42:07 +0530

    Update Program

--- CMD/CheckNumber.java
+++ CMD/CheckNumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Number{
     private int num;
     public Number(int num){
@@ -10,45 +12,44 @@ class Number{
         return (num<0);
     }
     public boolean isZero(){
-        if(num == 0)
-           return true;
-        return false;
+        return (num == 0);
     }
     public boolean isEven(){
-        if(num%2 == 0)
-           return true;
-        return false;
+        return (num % 2 == 0);
+       
     }
     public boolean isOdd(){
-        if(num%2 != 0)
-           return true;
-        return false;
+        return (num % 2 != 0);
+   
     }
 }
 
 class testNum{
     public static void main(String args[]){
-      
-        int len,num;
+        int len,num,ch;
         boolean ans;
         len = args.length;
         if(len == 1){
             try{
                 num = Integer.parseInt(args[0]);
                 Number ob = new Number(num);
-               
-                System.out.println("Number is Positive : "+ ob.isPositive());
-                    
-                System.out.println("Number is Negative : "+ob.isNegative());
+                    if(ob.isPositive())
+                        System.out.println("Number is Positive");
 
-                System.out.println("Number is Zero : "+ob.isZero());
-            
-                System.out.println("Number is Odd : "+ob.isOdd());
-                    
-                System.out.println("Number is Even : "+ ob.isEven());
-              
+                    if( ob.isNegative())
+                        System.out.println("Number is Negative");
+
+                    if(ob.isZero())
+                        System.out.println("Number is Zero");
+
+                    if(ob.isEven())
+                        System.out.println("Number is Even");
+
+                    if(ob.isOdd())
+                        System.out.println("Number is odd");
+
             }catch(Exception e){
-                System.out.println("Something Happened!");
+                System.out.println("Something Happened! Required Valid Integer Only");
             }
 
 
@@ -56,4 +57,5 @@ class testNum{
             System.out.println("Give one cmd-line argument");
         }
     }
-}
+}  
+
