fbc9eaebdda65eb8dc37e77934ba0af117e93c5a
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-30 15:52:13 +0530

    Correction in MathUtil package

--- GeoCode/MathUtil.java
+++ GeoCode/MathUtil.java
@@ -1,165 +1,172 @@
 import java.util.Scanner;
-class static Square{
-     private int a;
-     private int area;
-     private int perimeter;
-     private float diagonal;
 
-     public Square(int a){
+class Square {
+    private int a;
+    private int area;
+    private int perimeter;
+    private double diagonal;
+
+    public Square(int a) {
         this.a = a;
-     }
-     public void area(){
+    }
+
+    public void area() {
         area = a * a;
-     }
-     public void Perimeter(){
+    }
+
+    public void perimeter() {
         perimeter = 4 * a;
-     }
-     public void diagonal(){
+    }
+
+    public void diagonal() {
         diagonal = a * Math.sqrt(2);
-     }
-     public void display(){
-        System.out.println("Area of Square :"+Area);
-        System.out.println("Perimeter of Square :"+perimeter);
-        System.out.println("Diagonal of Square :"+diagonal);
+    }
 
-     }
-
-   
+    public void display() {
+        System.out.println("Area of Square : " + area);
+        System.out.println("Perimeter of Square : " + perimeter);
+        System.out.println("Diagonal of Square : " + diagonal);
+    }
 }
 
-class static Triangle{
-     private int base;
-     private int height;
-     private int a;
-     private int b;
-     private int c;
-     private float s;
+class Triangle {
+    private int base;
+    private int height;
+    private int a;
+    private int b;
+    private int c;
+    private double s;
 
-     private float area;
-     private float perimeter;
-     private float herons;
+    private double area;
+    private double perimeter;
+    private double herons;
 
-     public void accept(){
+    public void accept() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter base :");
+        System.out.print("Enter base : ");
         base = sc.nextInt();
-        System.out.print("Enter Height :");
+        System.out.print("Enter height : ");
         height = sc.nextInt();
-        
 
-        System.out.print("Enter side a :");
+        System.out.print("Enter side a : ");
         a = sc.nextInt();
-        System.out.print("Enter side b :");
+        System.out.print("Enter side b : ");
         b = sc.nextInt();
-        System.out.print("Enter side c :");
+        System.out.print("Enter side c : ");
         c = sc.nextInt();
+    }
 
+    public void area() {
+        area = 0.5 * base * height;
+    }
 
-     }
-     public void area(){
-        area = (1/2) * breath * height;
-     }
-     public void Perimeter(){
+    public void perimeter() {
         perimeter = a + b + c;
-     }
-     public void Herons(){
+    }
+
+    public void herons() {
         s = perimeter / 2.0;
-        herons = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-        
-     }
-     public void display(){
-        System.out.println("Area of Triangle :"+Area);
-        System.out.println("Perimeter of Triangle :"+perimeter);
-        System.out.println("herons of Triangle :"+herons);
+        herons = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
 
-     }
-
-   
+    public void display() {
+        System.out.println("Area of Triangle : " + area);
+        System.out.println("Perimeter of Triangle : " + perimeter);
+        System.out.println("Heron's Formula Area of Triangle : " + herons);
+    }
 }
 
-class static Cube{
-     private int a;
-     private int SA;
-     private int volume;
-     private float diagonal;
+class Cube {
+    private int a;
+    private int SA;
+    private int volume;
+    private double diagonal;
 
-     public Cube(int a){
+    public Cube(int a) {
         this.a = a;
-     }
-     public void surfaceArea(){
-        SA = 6* a * a;
-     }
-     public void volume(){
+    }
+
+    public void surfaceArea() {
+        SA = 6 * a * a;
+    }
+
+    public void volume() {
         volume = a * a * a;
-     }
-     public void diagonal(){
+    }
+
+    public void diagonal() {
         diagonal = a * Math.sqrt(3);
-     }
-     public void display(){
-        System.out.println("Area of Square :"+SA);
-        System.out.println("Perimeter of Square :"+volume);
-        System.out.println("Diagonal of Square :"+diagonal);
-     }
+    }
+
+    public void display() {
+        System.out.println("Surface Area of Cube : " + SA);
+        System.out.println("Volume of Cube : " + volume);
+        System.out.println("Diagonal of Cube : " + diagonal);
+    }
 }
 
-class static Cuboid{
+class Cuboid {
     private int l;
     private int b;
     private int h;
     private int SA;
     private int volume;
-    private float diagonal;
+    private double diagonal;
 
-    public Cuboid(int l,int b,int h){
+    public Cuboid(int l, int b, int h) {
         this.l = l;
         this.b = b;
         this.h = h;
     }
-      public void surfaceArea(){
-        SA = 2* (l*b + b*h + h*l)
-     }
-     public void volume(){
-        volume = l*b*h;
-     }
-     public void diagonal(){
-        diagonal = Math.sqrt(l*l + b*b + h*h);
-     }
-     public void display(){
-        System.out.println("Area of Square :"+SA);
-        System.out.println("Perimeter of Square :"+volume);
-        System.out.println("Diagonal of Square :"+diagonal);
-     }
+
+    public void surfaceArea() {
+        SA = 2 * (l * b + b * h + h * l);
+    }
+
+    public void volume() {
+        volume = l * b * h;
+    }
+
+    public void diagonal() {
+        diagonal = Math.sqrt(l * l + b * b + h * h);
+    }
+
+    public void display() {
+        System.out.println("Surface Area of Cuboid : " + SA);
+        System.out.println("Volume of Cuboid : " + volume);
+        System.out.println("Diagonal of Cuboid : " + diagonal);
+    }
 }
 
-class static Cone{
-    //slant heigth : slH
-    //Curved surface area : csa
-    //total surface area : tsa
+class Cone {
     private int r;
     private int h;
-    private static float slH;
-    private float csa;
-    private float tsa;
-    private float vol;
+    private double slH;
+    private double csa;
+    private double tsa;
+    private double vol;
 
-    public Cone(int r,int h){
+    public Cone(int r, int h) {
         this.r = r;
         this.h = h;
-        slH = Math.sqrt(r*r + h*h);
-    }
-    public void CSA(){
-        csa = 3.14f * r * slH;
-    }
-    public void TSA(){
-        tsa = 3.14f * r *(slH + r);
-    }
-    public void volume(){
-        vol = (1/3)* 3.14f * r * r * h;
+        slH = Math.sqrt(r * r + h * h);
     }
 
-    public void display(){
-        System.out.println("Curved Surface Area of cone :"+csa);
-        System.out.println("Total surface area of cone  :"+tsa);
-        System.out.println("Volume of Cone :"+vol);
-     }
+    public void csa() {
+        csa = Math.PI * r * slH;
+    }
+
+    public void tsa() {
+        tsa = Math.PI * r * (slH + r);
+    }
+
+    public void volume() {
+        vol = (1.0 / 3) * Math.PI * r * r * h;
+    }
+
+    public void display() {
+        System.out.println("Curved Surface Area of Cone : " + csa);
+        System.out.println("Total Surface Area of Cone : " + tsa);
+        System.out.println("Volume of Cone : " + vol);
+    }
 }
