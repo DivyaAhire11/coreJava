d068861ba95795c05c18ee2fa944d35b8d9f11f5
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-10 16:13:54 +0530

    Without Object Through method calling using Variables

--- DeepConcept/Interesting2.java
+++ DeepConcept/Interesting2.java
@@ -1,27 +1,37 @@
 /*
     I Love Java
+
+    Static variable :Learner
     I am Static Block
     I am main method
+
+    I Love C Lang
+
+    instance variable :1
     I am Init Block
     I am Constructor
 
  */
 
 public class Interesting2{
-    static int b = m2();
+    static String a = m2();
+    int b = m1();
     static{
+        System.out.println("Static variable :"+a);
        System.out.println("I am Static Block");
     }
     
     {
+        System.out.println("instance variable :"+b);
        System.out.println("I am Init Block");
     }
-    void m1(){
-       System.out.println("I am Instance method");
+    int m1(){
+       System.out.println("I Love C Lang");
+       return 1;
     }
-    static int m2(){
+    static String m2(){
         System.out.println("I Love Java");
-        return 1;
+        return "Learner";
     }
     Interesting2(){
         System.out.println("I am Constructor");
