1056d8c3eb4a6d40269ad7955bf2483eb85946a0
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-14 13:06:51 +0530

    Prime numbers find upto n

---
+++ Packages/PrimeSeries.java
@@ -0,0 +1,22 @@
+package series;
+
+public class PrimeSeries {
+    public static void printSeries(int x) {
+        System.out.println("\n----- Prime Series -----");
+        
+        for (int i = 2; i <= x; i++) {
+            boolean isPrime = true; // assume number is prime
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false; // found a divisor
+                    break;           // stop checking further
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + "\t");
+            }
+        }
+    }
+}
