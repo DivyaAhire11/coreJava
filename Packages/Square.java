6c1d50cba56e8358babdc6917172bf2cf1ff435c
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-14 10:46:52 +0530

    One file and one class

---
+++ Packages/Square.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+class Square{
+       int n;
+
+       public Square(int x){
+         n = x;
+       }      
+
+       public void printSquare(){
+         for(int i = 1;i <= n; i++){
+            System.out.print(""+i+ " = "+(i*i)+"\t");
+         }
+       }  
+   
+   
+   
+    public static void main(String []args){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter Number :");
+       int x = sc.nextInt();
+
+       Square ob = new Square(x);
+       System.out.println("Square Serirs ");
+       ob.printSquare();
+
+    }
+
+    
+}
