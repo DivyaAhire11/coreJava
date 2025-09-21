200e10fb33e8c2968a81c298fb408cb72c2aa282
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-21 23:26:30 +0530

    Manager Is A Employee using Inheritance Practice Problem

--- Packages/testCapital.java
+++ Packages/testCapital.java
@@ -3,17 +3,33 @@ import java.util.Scanner;
 class Capital{
       public static void main(String args[]){
         Scanner s = new Scanner(System.in);
-        String name,city;
-        System.out.print("Enter Your Full Name : ");
-        name = s.nextLine();  
-       
-        System.out.print("Enter City Name : ");
-        city = s.nextLine();
-        
-        String x[] = new String[5];
+        String name,city,x[];
+        int i,n; 
         CapitalString cs = new CapitalString();
-        x = cs.Capitalized(name);
-        Person p = new Person(name,city);
+
+        // System.out.print("Enter Your Full Name : ");
+        // name = s.nextLine();  
+       
+        // System.out.print("Enter City Name : ");
+        // city = s.nextLine();
+        System.out.print("Enter Who many person :");
+        n = s.nextInt();
+        Person pr[] = new Person[n]; //person
+        for(i=0;i<pr.length;i++){
+          System.out.print("Enter Your Full Name : ");
+          name = s.nextLine();  
+          System.out.print("Enter City Name : ");
+          city = s.nextLine();
+         
+         
+           pr[i] =  new person(name,city);
+        }
+      //   for(i=0;i<pr.length;i++){
+      //   String[] x = 
+      //  }
+        pr[i].name = cs.Capitalized(name);
+
+        // Person p = new Person(name,city);
         p.display(x);
     }
-}
+} 
