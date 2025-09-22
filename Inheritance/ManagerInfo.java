49d02cde8cba6306667e95a3cf612a2a867e4e61
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-22 09:18:57 +0530

    Using Array of Objects of Managers

--- Inheritance/ManagerInfo.java
+++ Inheritance/ManagerInfo.java
@@ -3,20 +3,31 @@ import java.util.Scanner;
 class ManagerInfo{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int id;
+        int i,id,n;
         String name,dept;
         float sal;
-        System.out.print("Enter Id :");
-        id = sc.nextInt();
-         System.out.print("Enter Name :");
-        name = sc.next();
-         System.out.print("Enter Department :");
-         dept = sc.next();
-         System.out.print("Enter Salary :");
-        sal = sc.nextFloat();
-        Manager m = new Manager(id,name,dept,sal);
-        m.accept();
-       System.out.println("ID\t\tNAME\t\tDAPARTMENT\tSALARY\t\tBONUS");
-        m.display();
+        System.out.print("Enter How Many managers : ");
+        n = sc.nextInt();
+
+        Manager[] mr = new Manager[n];
+
+        for(i = 0;i < mr.length; i++){
+            System.out.print("Enter Id :");
+            id = sc.nextInt();
+            System.out.print("Enter Name :");
+            name = sc.next();
+            System.out.print("Enter Department :");
+            dept = sc.next();
+            System.out.print("Enter Salary :");
+            sal = sc.nextFloat();
+            mr[i] = new Manager(id,name,dept,sal);
+            mr[i].accept();
+           
+        }
+        
+         System.out.println("ID\t\tNAME\t\tDAPARTMENT\tSALARY\t\tBONUS");
+         for(i = 0;i < mr.length; i++){
+            mr[i].display();
+         }
     }
 }
