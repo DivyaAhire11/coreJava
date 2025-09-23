b2c61e3426b38cfaef077961314d71c69621f226
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-23 20:22:32 +0530

    Using SWING create format of calculator

---
+++ GUIPrograms/MyFrame2.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+class MyFrame2 extends JFrame{
+      JLabel l1,l2,l3;
+      JTextField tf1,tf2,tf3;
+      JButton b1,b2,b3,b4;
+
+      MyFrame2(){
+        setTitle("Swing GUI");
+        setBounds(400,100,400,400);
+        setLayout(new FlowLayout());
+
+        //Create Component
+        l1 = new JLabel("Enter First Number ");
+        l2 = new JLabel("Enter Second Number ");
+        l3 = new JLabel("Result");
+        tf1 = new JTextField(20);
+        tf2 = new JTextField(20);
+        tf3 = new JTextField(20);
+        tf3.setEditable(false);
+        b1 = new JButton("ADD");
+        b2 = new JButton("SUB");
+        b3 = new JButton("MUL");
+        b4 = new JButton("DIV");
+
+        //Add Component
+        add(l1);
+        add(tf1);
+        add(l2);
+        add(tf2);
+        add(l3);
+        add(tf3);
+        add(b1);
+        add(b2);
+        add(b3);
+        add(b4);
+      
+        setVisible(true);
+
+      }
+}
+class TestFrame2{
+    public static void main(String args[]){
+        MyFrame2 f = new MyFrame2();
+    }
+}
