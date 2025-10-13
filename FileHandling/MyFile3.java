4a0441bde044e1f230170edbbe26732c32637d11
DivyaAhire11 <divyaahire121@gmail.com>
2025-10-13 12:03:27 +0530

    Delete SuccessFully

--- FileHandling/MyFile3.java
+++ FileHandling/MyFile3.java
@@ -1,9 +1,12 @@
 import java.io.*;
+import java.util.*;
 
 class MyFile{
     public static void main(String args[]){
+          Scanner sc = new Scanner(System.in);
             if(args.length == 1){
-              File f = new File(args[0]);
+              String fname = args[0];
+              File f = new File(fname);
               if(f. isFile()){
                  System.out.println(args[0]+" Is a File");
                  System.out.println("Hidden File??? :"+f.isHidden());
@@ -28,16 +31,28 @@ class MyFile{
                       cnt++;
                   }
                 }
+
                 System.out.println("\nTotal Number OF TXT files : "+cnt);
                
-              
+                String ch;
+               for(String c:content){
+                  File ff  = new File(fname+"\\"+c);
+                  if(ff.isFile() && c.endsWith("txt")){
+                    System.out.println(c + " is FOUND");
+                    System.out.println("Do you Want To DELETE (yes/no) :");
+                        ch = sc.next();
+                    if(ch.equalsIgnoreCase("yes")){
+                      ff.delete();
+                      System.out.println("DELETE SUCCESSFULLY");
+                     }
+                  }
                 }
-               
+              }
               
-            }else{
+            }else
                  System.out.println("Pass One File as an Argument.");
+            
             }
-    }
 }
 /*
 Demo IS a Directory
